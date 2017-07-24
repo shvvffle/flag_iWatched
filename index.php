939694adbce9a27dc13cab59a9d6a3abd9d6441e
Marina Marques <marina.marques@sherpany.com>
2017-07-24 15:41:34 +0100

    added favicon, css adjusments

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <title>iWatched</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
 </head>
